a5a47bddb0e078bad3f5b9996278569ff7166116
Thierry <thierryblankenstein2002@gmail.com>
2023-05-20 21:07:05 +0200

    kernel size experiment

---
+++ models/parabolic_lenet.py
@@ -0,0 +1,54 @@
+from torch.nn import Module
+from torch.nn import Conv2d
+from torch.nn import Linear
+from torch.nn import ReLU
+from torch.nn import LogSoftmax
+from torch import flatten
+from morphology_package.src.morphological_torch.pooling_operations import ParabolicPool2D_V2
+import torch
+
+torch.manual_seed(0)
+
+class LeNet(Module):
+	def __init__(self, numChannels, classes, ks):
+		# call the parent constructor
+		super(LeNet, self).__init__()
+		# initialize first set of CONV => RELU => POOL layers
+		self.conv1 = Conv2d(in_channels=numChannels, out_channels=20,
+			kernel_size=(5, 5))
+		self.relu1 = ReLU()
+		self.pool1 = ParabolicPool2D_V2(20, ks)
+		# initialize second set of CONV => RELU => POOL layers
+		self.conv2 = Conv2d(in_channels=20, out_channels=50,
+			kernel_size=(5, 5))
+		self.relu2 = ReLU()
+		self.pool2 = ParabolicPool2D_V2(50, ks)
+		# initialize first (and only) set of FC => RELU layers
+		self.fc1 = Linear(in_features=800, out_features=500)
+		self.relu3 = ReLU()
+		# initialize our softmax classifier
+		self.fc2 = Linear(in_features=500, out_features=classes)
+		self.logSoftmax = LogSoftmax(dim=1)
+
+	def forward(self, x):
+		# pass the input through our first set of CONV => RELU =>
+		# POOL layers
+		x = self.conv1(x)
+		x = self.relu1(x)
+		x, _ = self.pool1(x)
+		# pass the output from the previous layer through the second
+		# set of CONV => RELU => POOL layers
+		x = self.conv2(x)
+		x = self.relu2(x)
+		x, _ = self.pool2(x)
+		# flatten the output from the previous layer and pass it
+		# through our only set of FC => RELU layers
+		x = flatten(x, 1)
+		x = self.fc1(x)
+		x = self.relu3(x)
+		# pass the output to our softmax classifier to get our output
+		# predictions
+		x = self.fc2(x)
+		output = self.logSoftmax(x)
+		# return the output predictions
+		return output
